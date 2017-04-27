8e0687ee6becb79315e9eb3aeedca3951c61e946
Max Harris <mjharris@colorado.edu>
2017-04-27 01:14:42 -0500

    Icons & bottom tab bar switch active tab

--- components/BottomTabBar.js
+++ components/BottomTabBar.js
@@ -3,27 +3,48 @@ import {
   StyleSheet,
   View,
   Text,
-  Image
+  Image,
+  Button,
+  TouchableOpacity
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class BottomTabBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: 'activity'
+    }
+  }
+
+  _active(tab) {
+    return tab === this.state.activeTab ? styles.active : null;
+  }
+
+  _onPress(tab) {
+    this.setState({ activeTab: tab });
+  }
+
   render() {
     return (
       <View style={styles.wrapper}>
         <View style={styles.container}>
-          <View style={styles.tabContainer}>
-            <Text>On Top</Text>
-            <Text>First</Text>
-          </View>
-          <View style={styles.tabContainer}>
-            <Text>Second</Text>
-          </View>
-          <View style={styles.tabContainer}>
-            <Text>Third</Text>
-          </View>
-          <View style={styles.tabContainer}>
-            <Text>Fourth</Text>
-          </View>
+          <TouchableOpacity style={styles.tabContainer} onPress={() => this._onPress('activity')}>
+            <Icon name="ios-chatbubbles" style={[styles.icon, this._active('activity')]}/>
+            <Text style={[styles.subtext, this._active('activity')]}>Activity</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabContainer} onPress={() => this._onPress('tree')}>
+            <Icon name="md-git-network" style={[styles.icon, this._active('tree')]}/>
+            <Text style={[styles.subtext, this._active('tree')]}>Tree</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabContainer} onPress={() => this._onPress('add')}>
+            <Icon name="md-add" style={[styles.icon, this._active('add')]}/>
+            <Text style={[styles.subtext, this._active('add')]}>Add</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.tabContainer} onPress={() => this._onPress('profile')}>
+            <Icon name="md-person" style={[styles.icon, this._active('profile')]}/>
+            <Text style={[styles.subtext, this._active('profile')]}>Profile</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
@@ -43,5 +64,15 @@ const styles = StyleSheet.create({
     flexGrow: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  icon: {
+    fontSize: 30,
+    color: "#9B9B9B"
+  },
+  subtext: {
+    color: "#9B9B9B"
+  },
+  active: {
+    color: "#186CAA"
   }
 });
