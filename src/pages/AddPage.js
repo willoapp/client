d4c47341f01fa4938bfa10b6dd570940b1025962
Max Harris <mjharris@colorado.edu>
2017-06-09 16:40:21 -0500

    Use cloud development db

--- src/pages/AddPage.js
+++ src/pages/AddPage.js
@@ -9,17 +9,38 @@ import {
 import HeaderNav from '../components/HeaderNav';
 import colors from '../assets/styles/colors';
 
-import { GraphqlService } from '../services/graphql.service'
+import graphqlService from '../services/graphql.service'
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import postActions from '../actions/post.actions';
 
+function AddActivity(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (!isLoggedIn) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>No user profile yet...</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View style={{ padding: 20 }}>
+        <TextInput
+          autoFocus={true}
+          style={{height: 150, fontSize: 16}}
+          placeholder="What's on your mind?"
+          onChangeText={(text) => this.onTextChange(text)}
+          multiline={true}/>
+        <Button title="Post" onPress={() => this.addPost()}/>
+      </View>
+    )
+  }
+}
+
 class AddPage extends Component {
-  graphqlService = null;
 
   constructor(props) {
     super(props);
-    this.graphqlService = new GraphqlService();
   }
 
   onTextChange(text) {
@@ -28,7 +49,7 @@ class AddPage extends Component {
 
   addPost() {
     const post = { content: this.state.text, userId: "58d9eafafa8beec1b2c33cbb" }
-    this.graphqlService.mutate(`mutation ($post: PostInput!) { addPost(post: $post) { _id state content createdAt user {username} } }`, { post }).then(data => {
+    graphqlService.mutate(`mutation ($post: PostInput!) { addPost(post: $post) { _id state content createdAt user {username} } }`, { post }).then(data => {
       const post = data.addPost;
       this.props.actions.addPost(post);
     }).catch(error => console.log(error));
@@ -39,15 +60,7 @@ class AddPage extends Component {
       <View style={styles.container}>
         <HeaderNav/>
 
-        <View style={{ padding: 20 }}>
-          <TextInput
-            autoFocus={true}
-            style={{height: 150, fontSize: 16}}
-            placeholder="What's on your mind?"
-            onChangeText={(text) => this.onTextChange(text)}
-            multiline={true}/>
-          <Button title="Post" onPress={() => this.addPost()}/>
-        </View>
+        <AddActivity isLoggedIn={false}/>
 
       </View>
     );
